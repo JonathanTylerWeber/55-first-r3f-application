7bc5709b8240ebb6d45ecb17ed12b1425ccdc2e4
Jonathan Tyler Weber <jonathantweber@gmail.com>
2025-02-13 14:52:24 +0800

    done

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,31 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
+import Experience from "./Experience";
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-    <>
-        Soon to be a badass R3F application
-    </>
-)
+  <Canvas
+    // orthographic
+    // flat
+    // dpr={[1, 2]}
+    gl={
+      {
+        // antialias: false,
+        // toneMapping: THREE.CineonToneMapping,
+        // outputColorSpace: THREE.LinearEncoding,
+      }
+    }
+    camera={{
+      fov: 45,
+      // zoom: 100,
+      near: 0.1,
+      far: 200,
+      position: [3, 2, 6],
+    }}
+  >
+    <Experience />
+  </Canvas>
+);
